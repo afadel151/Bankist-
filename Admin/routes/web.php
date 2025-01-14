3f5b0e93df5995cdfd4ce5a85c10a04aa6b57525
afadel151 <af8356207@gmail.com>
2025-01-14 19:31:31 +0100

    Admin application init

--- Admin/routes/web.php
+++ Admin/routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
-use App\Http\Controllers\ClientController;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\UserController;
-use App\Models\Client;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -20,24 +17,7 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-Route::get('/users',function (){
-    return Client::all();
-});
-Route::middleware(['auth'])->group( function (){
 
-    Route::prefix('clients')->group(function ()  {
-        Route::get('/', [UserController::class, 'clients']);
-        Route::prefix('{id}')->group(function ()  {
-            Route::get('/', [ClientController::class, 'profile']);
-            Route::get('/deposit', [ClientController::class, 'deposit']);
-        });
-    });
-    Route::prefix('loans')->group(function ()  {
-        Route::get('/', [UserController::class, 'loans']);
-        Route::get('/update-status', [UserController::class, '']);
-    });
-    
-});
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
